b9ebd326d3654c1ffe308182f885f42d51e4ddeb
Kate <gutkati@email.ru>
2025-08-13 11:41:02 +0400

    убрать проверку из app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,14 @@ import colors from './styles/_variables.module.scss'
 
 export default function App() {
 
-    const isBuildTime = process.env.BUILD_TIME === 'true';
-    const isProd = process.env.NODE_ENV === 'production';
+    return (
+        <div className={styles.app}>
+            <Header/>
 
-    // Во время сборки (npm run build) показываем заглушку
-    if (isBuildTime) {
-        return <div>Загрузка данных...</div>;
-    }
+            <main className={styles.main}>
+                <ButtonLink color={colors.lightgreenColor} text="Токены" link='/token'/>
+            </main>
 
-   // В рантайме — грузим данные
-    if (isProd || process.env.NODE_ENV === 'development') {
-        return(
-            <div className={styles.app}>
-                <Header/>
-
-                <main className={styles.main}>
-                    <ButtonLink color={colors.lightgreenColor} text="Токены" link='/token'/>
-                </main>
-
-            </div>
-        );
-    }
+        </div>
+    );
 }
