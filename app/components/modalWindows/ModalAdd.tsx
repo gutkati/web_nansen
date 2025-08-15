c13d99c6e5e2ae547291e2732a205c215bfb167d
Kate <gutkati@email.ru>
2025-08-15 10:56:44 +0400

    Валидация формы добавления Токена

--- app/components/modalWindows/ModalAdd.tsx
+++ app/components/modalWindows/ModalAdd.tsx
@@ -9,29 +9,68 @@ type ModalAddProps = {
     // onConfirm: (id:number) => void;
 }
 
-const ModalAdd:React.FC<ModalAddProps> = ({title, onClose}) => {
+const ModalAdd: React.FC<ModalAddProps> = ({title, onClose}) => {
     const [link, setLink] = useState<string>('')
     const [errorLink, setErrorLink] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [errorName, setErrorName] = useState<string>('')
+    const [price, setPrice] = useState<string>('0')
+    const [errorPrice, setErrorPrice] = useState<string>('')
     let textErrorLink: string = 'Введите корректный URL'
+    let requiredField: string = 'Обязательное поле'
 
     const isValidUrl = (value: string) => {
         const pattern = /^(https?:\/\/)?([\w\-]+\.)+[a-z]{2,6}(:\d+)?(\/.*)?$/i;
         return pattern.test(value);
     };
 
-    const handleBlur = () => {
-        if (link.trim() && !isValidUrl(link)) {
+
+    const handleBlurLink = () => {
+        const trimmedLink = link.trim();
+
+        if (!trimmedLink.trim()) { // пустая строка или только пробелы
+            setErrorLink(requiredField);
+        } else if (!isValidUrl(trimmedLink.trim())) { // убираем пробелы перед проверкой
             setErrorLink(textErrorLink);
         } else {
             setErrorLink('');
         }
     };
 
+    const handleBlurName = () => {
+        const trimmedName = name.trim();
+
+         if (!trimmedName.trim()) {
+            setErrorName(requiredField)
+        } else if (trimmedName.length > 30) {
+            setErrorName('Имя не более 50 символов')
+        } else {
+            setErrorName('')
+        }
+    }
+
+    const handleBlurPrice = () => {
+        const num = Number(price);
+        if (!price.trim()) {
+            setErrorPrice('Введите цену');
+        } else if (num < 0) {
+            setErrorPrice('Введите положительное число');
+        } else if (num === 0) {
+            setErrorPrice('Введите цену');
+        } else if (num < 1) {
+            setErrorPrice('Введите корректную цену');
+        } else if (/^0\d+/.test(price)) {
+            setErrorPrice('Число не должно начинаться с нуля');
+        } else {
+            setErrorPrice('');
+        }
+    }
+
 
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault()
 
-        if(!isValidUrl(link)) {
+        if (!isValidUrl(link)) {
             setErrorLink(textErrorLink);
             return;
         }
@@ -41,16 +80,45 @@ const ModalAdd:React.FC<ModalAddProps> = ({title, onClose}) => {
     return (
         <form className={styles.form__add} onSubmit={handleSubmit}>
             <h3 className={styles.add__title}>{title}</h3>
-            <label className={styles.add__label}>Вставьте ссылку токена
+
+            <label className={styles.add__label}>Ссылка токена
                 <input
                     className={styles.add__input}
                     type="text"
                     value={link}
                     onChange={(e) => setLink(e.target.value)}
-                    onBlur={handleBlur}
+                    onBlur={handleBlurLink}
                     placeholder='Ссылка'/>
                 <span className={styles.add__error}>{errorLink}</span>
             </label>
+
+            <div className={styles.add__container}>
+                <label className={styles.add__label}>Имя токена
+                    <input
+                        className={styles.add__input}
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        onBlur={handleBlurName}
+                        placeholder='Имя'/>
+                    <span className={styles.add__error}>{errorName}</span>
+                </label>
+
+                <label className={styles.add__label}>Цена сделки
+                    <input
+                        className={styles.add__input}
+                        type="number"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        onBlur={handleBlurPrice}
+                        onFocus={() => {
+                            if (price === '0') setPrice(''); // убираем 0 при фокусе
+                        }}
+                        placeholder='Цена'/>
+                    <span className={styles.add__error}>{errorPrice}</span>
+                </label>
+            </div>
+
 
             <div className={styles.add__btns}>
                 <button
