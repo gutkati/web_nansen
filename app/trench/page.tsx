ccc009616c894490aef2d57585b759f93964fba4
Kate <gutkati@email.ru>
2025-09-10 12:36:50 +0400

    Продлить оповещение/Добавить кнопку раздела Trench tokens

---
+++ app/trench/page.tsx
@@ -0,0 +1,27 @@
+import Token from "@/app/token/Token";
+import type {Metadata} from "next";
+import {getTokens} from "@/lib/queries/tokenQueries";
+import {getPurchasesAll} from "@/lib/queries/listAllPurchasesQueries"
+import {getLastPurchase} from "@/lib/queries/listAllPurchasesQueries";
+
+// делает страницу динамической
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+    title: "Токены",
+    description: "Список токенов",
+};
+
+export default async function TokenPage() {
+        // const tokens = await getTokens();
+        // const listPurchases = await getPurchasesAll();
+        // const lastPurchase = await getLastPurchase();
+        //
+        // return (
+        //     <Token
+        //         tokens={tokens}
+        //         listPurchases={listPurchases}
+        //         lastPurchase={lastPurchase}
+        //     />
+        // );
+}
